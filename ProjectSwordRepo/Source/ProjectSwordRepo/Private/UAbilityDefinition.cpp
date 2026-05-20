3c66eac1c78231b878ee161710cd757761d79b0a
Bruno de Souza <brunosouza502@gmail.com>
2026-05-20 12:08:55 -0300

    Create UAbilityDefinition.cpp

---
+++ ProjectSwordRepo/Source/ProjectSwordRepo/Private/UAbilityDefinition.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UAbilityDefinition.h"
+
+/*UAbilityDefinition::UAbilityDefinition()
+{
+}
+
+UAbilityDefinition::~UAbilityDefinition()
+{
+}*/
